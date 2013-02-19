e7df9f201485207fcbbdd25542c72a5df36924c8
John Barton (joho) <jrbarton@gmail.com>
2013-02-19 18:40:06 +1100

    Have some caching charlie.

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'thin'
 gem 'sinatra'
 gem 'sinatra-cross_origin'
 gem 'sinatra-contrib'
+gem 'sinatra-dalli'
 gem 'json'
 
 group :development do
--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 # app.rb
 require 'sinatra'
 require 'sinatra/cross_origin'
+require 'sinatra/dalli'
 require 'json'
 
 # dev mode easy lols
@@ -18,14 +19,37 @@ before do
   response.headers["Access-Control-Allow-Headers"] = "x-requested-with"
 end
 
-lyrics = [
-  {:line => 'It was dark', :time => 1},
-  {:line => 'In the car park', :time => 2},
-  {:line => 'I heard a lark', :time => 3},
-  {:line => 'Ascending', :time => 3.5}
-]
+def lyrics
+  # 30ish of these
+  [
+    {:line => 'It was dark', :time => 1},
+    {:line => 'In the car park', :time => 2},
+    {:line => 'I heard a lark', :time => 3},
+    {:line => 'Ascending', :time => 3.5}
+  ]
+end
+
+def tweet_for_lyric(lyric)
+  {:text => "", :link => "", :tweeter => "", :created_at => ""}
+end
 
 get '/' do
   content_type :json
-  lyrics.to_json
+
+  cache "lyrics_json" do
+    lyrics.map { |lyric|
+      lyric[:tweet] = tweet_for_lyric(lyric)
+      lyric
+    }.to_json
+  end
 end
+
+# for debugging, delete before deploying
+get '/cache/expire' do
+  expire "lyrics_json"
+end
+
+get '/cache/inspect' do
+  send(:client).inspect
+end
+# end debugging code
