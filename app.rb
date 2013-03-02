9fceec6fe93b80cc8600b094ca7155dd65ffc56e
Charlie Gleason <hi@superhighfives.com>
2013-03-02 23:22:16 +1100

    Removed a slash.

--- app.rb
+++ app.rb
@@ -12,7 +12,7 @@ configure :development do
 end
 
 # cross domain
-set :allow_origin, ['http://localhost:3501', 'http://tweetflight.wearebrightly.com.s3-website-ap-southeast-2.amazonaws.com/', 'http://tweetflight.wearebrightly.com']
+set :allow_origin, ['http://localhost:3501', 'http://tweetflight.wearebrightly.com.s3-website-ap-southeast-2.amazonaws.com', 'http://tweetflight.wearebrightly.com']
 set :allow_methods, [:get, :post, :options]
 set :allow_credentials, false
 
